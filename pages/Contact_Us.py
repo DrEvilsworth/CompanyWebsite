dec58534240bf73f3e37d7555b51048eae9627de
nathan-barron <nathan.barron@employbridge.com>
2024-05-30 19:35:18 -0400

    created email sending form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,28 @@
 import streamlit as st
+import send_email
+import pandas as pd
 
-st.header('Contact Us')
+df = pd.read_csv('files/topics.csv')
+
+st.header('Contact Me')
+
+
+
+
+
+with st.form(key='email_form'):
+    user_email = st.text_input("Your Email Address")
+
+    user_selection = st.selectbox('What topic do you want to discuss?', df['topic'])
+    init_message = st.text_area("Your Message")
+    message = f"""\
+Subject: New Email from {user_email}
+From: {user_email}
+Topic {user_selection}
+{init_message}
+"""
+
+    button = st.form_submit_button('Submit')
+    if button:
+        send_email.sendemail(message, user_email)
+        st.info("Your email was sent successfully")
